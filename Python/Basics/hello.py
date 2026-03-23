eb146823dd41c57fce18dd71075886363db1ad6e
Vamshi S <vomshiii@gmail.com>
2026-03-23 21:02:55 +0530

    Create Basic code  and escape sequences practice

---
+++ Python/Basics/hello.py
@@ -0,0 +1,21 @@
+# print('Hello World')
+print('Just simple hello world')
+
+print('hello "world"') # "" inside the '' can work but '' inside '' wont work
+
+#use \ before ' or " at starting and \ before end character of " or '
+print("hello \"world\"") 
+print('hello \'world\'')
+
+# "\\" --> helpful to print the \ in the message.
+print("Path: C:\\python\\Basics\\hello.py") 
+
+#very messy in the single line
+print('Your learning path : \n\t-Python Basics \n\t-Data Engineering \n\t-AI')
+
+# """ it eases to write the message in multiple line where it has in built /n for every new line we write 
+print("""Our Learning Path :
+      \t-Python Basics
+      \t-Data Engineering
+      \t-AI""")
+
