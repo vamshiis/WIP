1ba4b85e0c8c10624570238c8344eee800874048
Vamshi S <vomshiii@gmail.com>
2026-03-29 20:10:26 +0530

    Add String's Type operations

--- Python/DataTypes/Strings/Types.py
+++ Python/DataTypes/Strings/Types.py
@@ -7,4 +7,4 @@ age = 23
 print(type(age))
 print('Your age is:' + age) #can't combine a string with an integer using + operator.
 print('your age is :'+str(age)) # str() converts any data into str data type. Its done to just execute this line it wont change in the memory.
-# age=str(age) --> this leads to change in memory completely converted from int to str data type.
+age=str(age) # this leads to change in memory completely converted from int to str data type.
