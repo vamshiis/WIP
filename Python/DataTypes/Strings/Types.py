70cb15500e38636af48b47db5de23e2803e4726a
Vamshi S <vomshiii@gmail.com>
2026-03-29 20:06:06 +0530

    Add Strin's Types operations.

--- Python/DataTypes/Strings/Types.py
+++ Python/DataTypes/Strings/Types.py
@@ -1,10 +1,10 @@
 # Types category :
 # Built in functions in Strings
-# name = "Vamshi"
-# # Type
-# print(type(name))
-# age = 23
-# print(type(age))
-# # print('Your age is:' + age) can't combine a string with an integer using + operator.
-# print('your age is :'+str(age)) # str() converts any data into str data type. Its done to just execute this line it wont change in the memory.
-# # age=str(age) --> this leads to change in memory completely converted from int to str data type.
+name = "Vamshi"
+# Type
+print(type(name))
+age = 23
+print(type(age))
+print('Your age is:' + age) #can't combine a string with an integer using + operator.
+print('your age is :'+str(age)) # str() converts any data into str data type. Its done to just execute this line it wont change in the memory.
+# age=str(age) --> this leads to change in memory completely converted from int to str data type.
