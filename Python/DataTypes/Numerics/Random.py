133e7ccf8bd8db77c9d76f176e56c600f2c3d026
Vamshi S <vomshiii@gmail.com>
2026-03-30 14:41:41 +0530

    Add Numerics Random functions Operations

--- Python/DataTypes/Numerics/Random.py
+++ Python/DataTypes/Numerics/Random.py
@@ -19,3 +19,17 @@ print(random.randint(1,6))
     The above both functions are used to generate test data (dummy) for like age,ID,or prices.
     Random Sampling - picking a smaller,random part of huge dataset
 '''
+
+# Challenge
+# Generate the random integer between 1 to 100, and check if the result is an even number.
+x = random.randint(1,100)
+print(f"{x} is generated")
+# Normal if - else block
+if x % 2 == 0: 
+    print(f"{x} is a even Number")
+else:
+    print(f"{x} is not an even number" ) 
+
+# # Ternary if - else block
+generated_value = f'{x} is even Number' if x % 2 == 0 else f'{x} is not even Number'   
+print(generated_value)
