ba27d0b19744477585490b345c9128c35731c35a
Vamshi S <vomshiii@gmail.com>
2026-03-30 14:16:19 +0530

    Add Numeric Random functions operations

---
+++ Python/DataTypes/Numerics/Random.py
@@ -0,0 +1,21 @@
+import random
+''' random() :
+    Syntax : random()
+    random function
+    output : float
+    - returns a random float between 0.0 and 1.0 
+'''
+print(random.random())
+
+''' randint() :
+    Syntax : randint(start,end)
+    random function
+    output : int
+    - gets a random whole number from start to end(both included)
+'''
+print(random.randint(1,6))
+
+''' 
+    The above both functions are used to generate test data (dummy) for like age,ID,or prices.
+    Random Sampling - picking a smaller,random part of huge dataset
+'''
