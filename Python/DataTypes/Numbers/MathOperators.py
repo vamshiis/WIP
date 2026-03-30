ae00880dc90b000703942d78a6adab59f25a847f
Vamshi S <vomshiii@gmail.com>
2026-03-30 11:32:43 +0530

    Add Numeric Math Operators Operations

---
+++ Python/DataTypes/Numbers/MathOperators.py
@@ -0,0 +1,21 @@
+print(2 + 3)
+print(5 - 3)
+print(2 * 3)
+print(7 / 2)
+# // floor division - It divides two numbers and rounds down
+print(7 // 2)
+#  remainder - The left over part after division - used to check if a number is even
+print(10 % 2)
+#  ** exponentiation - It raises a number to the power of another number
+print(2 ** 3)
+
+# shortcuts
+''' shorthand assignment
+    syntax : <operator>=
+    - apply an operator and reassign the result in one step 
+'''
+x = 3
+y = x+2
+x += 2
+print(x)
+print(y)
