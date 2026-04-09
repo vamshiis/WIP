fff91eb5321b03071e5f6669a65df02e18e0ab83
Vamshi S <vomshiii@gmail.com>
2026-04-09 12:24:08 +0530

    Add List creation Logic

---
+++ Python/Data_Structures/List/create.py
@@ -0,0 +1,54 @@
+''' ---------- How to Create ? ---------'''
+# Empty list
+empty = []
+print(empty)
+print(type(empty))
+
+# ---- Lists with similiar data types and manual item entry ----
+# List with String items --> Adding items to the list manually there can be other ways too to add items.
+names = ['vamshi', 'vamsh', 'john']
+print(names)
+print(type(names))
+
+# List with Number's item's
+numbers = [1, 2, 3]
+print(numbers)
+
+# ---- Lists with Mixed Data Types ------
+mixed_list = [1, 'a', 'True', None]
+print(mixed_list)
+
+''' Creation of List with Built-in function list()
+    list(value)
+    built - in function
+    output : list
+    - Convert an iterable (sequence) into a list
+'''
+empty = list()
+print(empty)
+
+letters = list('Vamshi')
+print(letters)
+
+
+numbers = list(range(5))
+print(numbers)
+print(len(numbers))
+
+# --------- Nested Lists - Matrix --------------
+# Similiar Data Types Nested List
+matrix = [
+    [1, 2, 2],
+    [3, 2, 4],
+    [7, 9, 0]
+]
+print(matrix)
+print(type(matrix))
+
+# mixed Nested List
+mixed_matrix = [
+    [1, 2, 3],
+    ['vamshi', 'john', 'unknown'],
+    [True, False]
+]
+print(mixed_matrix)
