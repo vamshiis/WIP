d5a5a8fb2888b7f4e006a3036ff5922991ca10ef
Vamshi S <vomshiii@gmail.com>
2026-04-13 10:25:23 +0530

    Add enumerate() Iterator Function logic

--- Python/Data_Structures/List/Advanced/iterator.py
+++ Python/Data_Structures/List/Advanced/iterator.py
@@ -17,3 +17,36 @@ print(next(new)) #2
 print(next(new)) #3
 print(next(new)) #StopIteration
 
+''' enumerate() :
+    function
+    Syntax : enumerate(iterable , start = 0)
+    output : Iterator
+    - enumerate is a iterator which we can use in for loop to iterate over items.
+    - by default the start is 0 we can specify start as we wish
+    - Enumerate use case :
+      - Find the exact position of the bad data in the list.
+      
+'''
+letters = ['a', 'b', 'c']
+# print(enumerate(letters))  # Returns <enumerate object at 0x00000269EAAC5D00>
+
+# To see the content in the object we need to wrap around list / other data structures(which calls repeatedly next() to store in specified Data structure)
+# or use in for loop which uses internal next().
+# print(list(enumerate(letters))) # when used list() it's like running the next() repeateadly to get all items and store in the list.
+
+# When the __next__() method is called on any iterator, it can only return exactly one object. It cannot return multiple independent variables at the same time.
+# so when internally in the for loop next() is called on enumerate, it will return a tuple object. which consist of 2 values in int (index,value)
+# Returns each time an tuple object
+for package in enumerate(letters):
+    print(package)
+
+# Iterable Unpacking. Any iterable object (Tuples, Lists, Strings) can be unpacked into multiple variables in a single line of code.
+# enumerate returns the tuple for each single loop which is iterable and iterable unpacking can be done on the same line.
+# So, as long as variables equal to values we can unpack things easily.
+for i, l in enumerate(letters):
+    print(i, l)
+
+# assign ids starting from 101
+employees = ['anna', 'bishop', 'catlyn']
+for id, employee in enumerate(employees, start=101):
+    print(id, employee)
