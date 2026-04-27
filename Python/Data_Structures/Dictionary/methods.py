1c12fbba3b8e54c8192b7edd1ca6c11fb37a7846
Vamshi S <vomshiii@gmail.com>
2026-04-27 15:17:46 +0530

    Add Viewing objects methods

--- Python/Data_Structures/Dictionary/methods.py
+++ Python/Data_Structures/Dictionary/methods.py
@@ -28,3 +28,47 @@ print(user.get('name','Unknown'))
 print('age' in user)
 print('name' not in user)
 
+''' Viewing Objects : 
+    - gives us a live view of the dictionary's keys,values, or key value pairs
+    - In Python, .keys(), .values(), and .items() are all Dictionary Views.
+    What is a "View"?
+    - A view is a live window into the dictionary's internal data.
+    - It does not create a new list or a separate copy of the keys.
+    - It stays linked to the dictionary.
+    - The 3 Golden Rules of View Objects
+        1. Dynamic          : If the dictionary changes, the view updates instantly.
+        2. Memory Efficient : It uses memory because it just "looks" at the dictionary's existing hash table.
+        3. Set-Like         : Specifically, dict_keys and dict_items support set operations like & (intersection) and | (union).
+'''
+''' keys() :
+    Syntax : variable.keys()
+    Output : dict_keys View Object
+    - Returns all the KEYS in our dictionary
+'''
+print(user.keys())
+
+''' values() :
+     Syntax : variable.values()
+     Output : dict_values View object  
+    - Returns all the VALUES in our dictionary
+'''
+print(user.values())
+
+''' items() :
+    Syntax : variable.items()
+    output : dict_items view object
+    - Returns all (key,value) pairs of our dictionary
+    -  Each element inside the view is a Tuple(like (id, 1))
+    - This method will be perfect when we need key and value together for
+      looping, transforming data, building new dicts, comparing and more
+'''
+print(user.items())
+
+''' print(user) - output : {'id': 1, 'age': 25, 'city': 'peru'}
+   - we can use normal print(users) to get items of dictionary
+   - But if we do it we get them in dictionary form which will be useful to display
+   - we cant able to loop around that output or perform any operations with it just.
+   - so we need to always use variable.items() so that we can use that result to loop,compare,transform and more.
+'''
+# print(user)
+
