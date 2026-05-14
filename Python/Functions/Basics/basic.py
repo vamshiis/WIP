0e5230a6a725fb7c8c762626791ed8ac6f662010
Vamshi S <vomshiii@gmail.com>
2026-05-14 10:39:50 +0530

    Add Basic functions

---
+++ Python/Functions/Basics/basic.py
@@ -0,0 +1,13 @@
+import math
+
+# Built-in Function (Just calling)
+print(len('Python'))
+
+# Function From Libraries (Import Then Call)
+number = 4.5
+print(math.ceil(number))
+
+# User Defined Function (Define Then Call)
+def greet():
+    print('Hello')
+greet()    
