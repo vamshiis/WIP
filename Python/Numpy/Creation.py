10368ed89ace4a8b9081550bad0f1fd3cec8ad68
Vamshi S <vomshiii@gmail.com>
2026-05-18 16:29:17 +0530

    Add Array creation using arange() function

--- Python/Numpy/Creation.py
+++ Python/Numpy/Creation.py
@@ -13,4 +13,19 @@ arr_tuple = np.array((10,20,30))
 print(arr_list)
 print(arr_tuple)
 
+''' Using Numerical Ranges : '''
+''' 1. arange() :
+    Function
+    Syntax : np.arange(start, stop, step, dtype = None)
+             start: Start of interval (defaults to 0).
+             stop: End of interval (exclusive).
+             step: Spacing between values. (default to 0)
+    output : numpy.ndarray of evenly spaced values.
+'''
+arr_range1 = np.arange(11) # 0,1,2,3,4,5,6,7,8,9,10
+arr_range2 = np.arange(1,6) # 1,2,3,4,5
+arr_range3 = np.arange(0,11,2) #0,2,4,6,8,10
+print(arr_range1)
+print(arr_range2)
+print(arr_range3)
 
